1ed4a95283ed92c82f34566f6bf5c9beb8fe2dc2
Anudeep <anudeepghosh01@gmail.com>
2016-03-31 21:32:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/anudeep/neighbuyer/Data.java
@@ -0,0 +1,79 @@
+package com.example.anudeep.neighbuyer;
+
+/**
+ * Created by Anudeep on 31-03-2016.
+ */
+public class Data {
+    String firstName;
+    String lastName;
+    String ID;
+    String phone;
+    String email;
+    String city;
+    double userLatitude;
+    double userLongitude;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getUserLatitude() {
+        return userLatitude;
+    }
+
+    public void setUserLatitude(double userLatitude) {
+        this.userLatitude = userLatitude;
+    }
+
+    public double getUserLongitude() {
+        return userLongitude;
+    }
+
+    public void setUserLongitude(double userLongitude) {
+        this.userLongitude = userLongitude;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
---
+++ app/src/main/java/com/example/anudeep/neighbuyer/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.anudeep.neighbuyer;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+String city="New Delhi";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Spinner cityChooser = (Spinner) findViewById(R.id.city);
+        ImageButton location = (ImageButton) findViewById(R.id.location);
+        EditText fstName = (EditText)findViewById(R.id.fstName);
+        EditText lstName = (EditText)findViewById(R.id.lstName);
+        EditText email = (EditText)findViewById(R.id.email);
+        EditText phone = (EditText)findViewById(R.id.phone);
+        //EditText fName = (EditText)findViewById(R.id.fstName);
+        setSupportActionBar(toolbar);
+        Data data = new Data();
+        cityChooser.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView adapter, View view, int position, long id) {
+                city = adapter.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        data.setCity(city);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
